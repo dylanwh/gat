c41cfa6c1d3e8f4106509ad574ef12b0b4badcab
Dylan William Hardison <dylan@hardison.net>
2010-11-28 23:38:12 -0500

    cleaned up path allow/deny system

--- lib/Gat/Path.pm
+++ lib/Gat/Path.pm
@@ -7,39 +7,32 @@ use Path::Class 'dir';
 use List::MoreUtils 'first_value';
 
 use MooseX::Params::Validate;
-use MooseX::Types::Moose ':all';
-use MooseX::Types::Structured 'Tuple';
 use MooseX::Types::Path::Class ':all';
 
+use Gat::Path::Rules;
 use Gat::Types ':all';
-use Gat::Path::File;
 
 has 'rules' => (
-    traits  => ['Array'],
-    isa     => ArrayRef [ Tuple [RegexpRef, Bool] ],
-    reader  => '_rules',
-    handles => { 'rules' => 'elements' },
-    default => sub { [] },
-);
-
-has 'rule_default' => (
-    is       => 'ro',
-    isa      => Bool,
-    default  => 1,
+    is         => 'ro',
+    isa        => 'Gat::Path::Rules',
+    lazy_build => 1,
 );
 
 has 'work_dir' => (
-    is       => 'ro',
-    isa      => AbsoluteDir,
-    builder  => '_build_work_dir',
-    lazy     => 1,
+    is      => 'ro',
+    isa     => AbsoluteDir,
+    coerce  => 1,
+    lazy    => 1,
+    builder => '_build_work_dir',
+    handles => { work_file => 'file' },
 );
 
 has 'base_dir' => (
     is      => 'ro',
     isa     => AbsoluteDir,
-    builder => '_build_base_dir',
     lazy    => 1,
+    builder => '_build_base_dir',
+    handles => { base_file => 'file' },
 );
 
 has 'gat_dir' => (
@@ -58,6 +51,7 @@ has 'gat_dir_name' => (
     isa     => 'Str',
     default => '.gat',
 );
+
 
 sub _build_work_dir { dir('.')->absolute }
 
@@ -79,7 +73,45 @@ sub _build_gat_dir {
     my ($self) = @_;
 
     return $self->base_dir->subdir($self->gat_dir_name);
+
 }
+
+sub _build_rules {
+    my ($self) = @_;
+    my $file = $self->gat_file('rules.yml');
+
+    if (-f $file) {
+        return Gat::Path::Rules->load($file);
+    }
+    else {
+        return Gat::Path::Rules->new;
+    }
+}
+
+sub save_rules {
+    my ($self) = @_;
+    my $file = $self->gat_file('rules.yml');
+
+    $self->rules->store( $file );
+}
+
+sub cleanup {
+    my $self = shift;
+    my ($file) = pos_validated_list( \@_, { isa => File, coerce => 1 } );
+
+    my @dirs;
+    for my $dir ( File::Spec->splitdir($file->stringify) ) {
+        if ( $dir eq '..' ) {
+            pop @dirs if @dirs;
+        }
+        else {
+            push @dirs, $dir;
+        }
+    }
+
+    return Path::Class::File->new(File::Spec->catdir(@dirs));
+}
+
 
 sub relative {
     my $self   = shift;
@@ -92,7 +124,7 @@ sub absolute {
     my $self   = shift;
     my ($file) = pos_validated_list(\@_, { isa => File, coerce => 1 });
 
-    return $file->absolute( $self->work_dir );
+    return $self->cleanup( $file->absolute( $self->work_dir ) );
 }
 
 sub canonical {
@@ -115,13 +147,9 @@ sub is_allowed {
     my ($file) = pos_validated_list(\@_, { isa => File, coerce => 1 });
     my $cfile  = $self->canonical( $file );
 
-    my $rule   = first_value { $cfile =~ $_->[0] } $self->rules;
-    return $rule ? $rule->[1] : $self->rule_default;
+    my $pred   = first_value { $cfile =~ $_->[0] } $self->rules->predicates;
+    return $pred ? $pred->[1] : $self->rules->default;
 }
-
-__PACKAGE__->meta->make_immutable;
-1;
-
 
 __PACKAGE__->meta->make_immutable;
 
