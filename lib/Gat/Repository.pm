7274da7304b570c6a7246ebc9fa0ed76f16c8fff
Dylan William Hardison <dylan@hardison.net>
2010-11-30 23:22:08 -0500

    work Repository API

--- lib/Gat/Repository.pm
+++ lib/Gat/Repository.pm
@@ -1,4 +1,4 @@
-package Gat::Repository::Local;
+package Gat::Repository;
 
 # ABSTRACT: The (default) asset method (store files in $GAT_DIR/asset)
 
@@ -7,6 +7,7 @@ use namespace::autoclean;
 
 use File::Copy::Reliable 'move_reliable';
 use File::Basename;
+use Digest;
 
 use Data::Stream::Bulk::Path::Class;
 use Data::Stream::Bulk::Filter;
@@ -18,7 +19,11 @@ use MooseX::Params::Validate;
 use Gat::Types ':all';
 use Gat::Error;
 
-with 'Gat::Repository::API';
+has 'digest_type' => (
+    is      => 'ro',
+    isa     => Str,
+    default => 'MD5',
+);
 
 has 'asset_dir' => (
     is       => 'ro',
@@ -33,18 +38,38 @@ has 'use_symlinks' => (
     required => 1,
 );
 
-sub _resolve_safe {
-    my ($self, $checksum) = @_;
-    my $prefix = substr($checksum, 0, 2);
-    return $self->asset_dir->subdir($prefix)->file(substr($checksum, 2));
+requires 'insert', 'link', 'remove', 'verify', 'assets';
+
+sub compute_checksum {
+    my $self   = shift;
+    my ($file) = pos_validated_list(\@_, { isa => File, coerce => 1 });
+
+    my $digest = Digest->new($self->digest_type);
+    my $fh     = $file->openr;
+    $digest->addfile($fh);
+    return $digest->hexdigest;
 }
 
 sub _resolve {
     my ($self, $checksum) = @_;
-    my $file = $self->_resolve_safe($checksum);
-    Gat::Error->throw( message => "missing asset $file" )  unless -f $file;
-    return $file;
+    my $prefix = substr($checksum, 0, 2);
+
+    return $self->asset_dir->subdir($prefix)->file(substr($checksum, 2));
 }
+
+
+sub fetch {
+    my ($self, $checksum) = @_;
+    my $file = $self->_resolve($checksum);
+    
+    if (-f $file) {
+        return $file;
+    }
+    else {
+        Gat::Error->throw( message => "missing asset $file" )  unless -f $file;
+    }
+}
+
 
 sub insert {
     my $self = shift;
@@ -57,7 +82,7 @@ sub insert {
     Gat::Error->throw(message => "$file is not a regular file") unless -f _;
 
     my $checksum   = $self->compute_checksum($file);
-    my $asset_file = $self->_resolve_safe($checksum);
+    my $asset_file = $self->_resolve($checksum);
 
     if (-f $asset_file) { # already have that, so remove it.
         unlink($file);
@@ -67,6 +92,7 @@ sub insert {
         move_reliable($file, $asset_file);   # move the file over.
     }
 
+    $self->link($file, $checksum);
     return $checksum;
 }
 
@@ -80,7 +106,7 @@ sub link {
 
     Gat::Error->throw( message => "Can't overwrite $file" ) if -e $file;
 
-    my $asset_file = $self->_resolve($checksum);
+    my $asset_file = $self->fetch($checksum);
     $file->parent->mkpath;
 
     if ($self->use_symlinks) {
@@ -91,22 +117,16 @@ sub link {
     }
 }
 
-sub unlink {
+sub remove {
     my $self = shift;
-    my ($file, $checksum) = pos_validated_list(
+    my ($checksum) = pos_validated_list(
         \@_,
-        { isa => AbsoluteFile, coerce => 1 },
         { isa => Checksum },
     );
 
-    Gat::Error->throw(message => "$file does not exist")        unless -e $file;
-    Gat::Error->throw(message => "$file is not a regular file") unless -f _;
-
     my $asset_file = $self->_resolve($checksum);
-    if ($self->compute_checksum($file) ne $checksum) {
-        Gat::Error->throw( message => "Cannot unlink unmanaged file: $file" );
-    }
-    unlink( $file );
+
+    unlink($asset_file) if -f $asset_file;
 }
 
 sub verify {
